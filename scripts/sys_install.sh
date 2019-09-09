5a8fc328f2618506ac5b7a082dc7712868911d58
Max O'Cull <max.ocull@protonmail.com>
2019-09-08 23:33:44 -0400

    Start OpenVPN with errors

--- scripts/sys_install.sh
+++ scripts/sys_install.sh
@@ -9,6 +9,8 @@ else
     source "/usr/local/lib/helm/scripts/sys_lib.sh"
 fi
 
+# TODO: Docker permissions for $USER?
+
 elevate mkdir -p /usr/local/lib/helm/
 elevated_link_source "${GITROOT}/scripts/" "/usr/local/lib/helm/scripts"
 elevated_link_source "${GITROOT}/helm" "/usr/local/bin/helm"
@@ -16,13 +18,20 @@ elevated_link_source "${GITROOT}/helm" "/usr/local/bin/helm"
 if [[ "$(probe dnf)" -eq 1 ]]; then
     elevate dnf upgrade -y
     elevate dnf install -y \
-        docker docker-compose tmux neovim nodejs rsync
+        docker docker-compose tmux neovim nodejs rsync htop git
 elif [[ "$(probe apt)" -eq 1 ]]; then
     elevate apt update
     elevate apt upgrade -y
     # TODO: docker-compose nodejs
     elevate apt install -y \
-        docker tmux neovim rsync
+        docker tmux neovim rsync htop
+fi
+
+# Gotop
+if [[ "$(probe gotop)" -eq 0 ]]; then
+    git clone --depth 1 https://github.com/cjbassi/gotop /tmp/gotop
+    /tmp/gotop/scripts/download.sh
+    elevate mv ./gotop /usr/local/bin/
 fi
 
 elevate mkdir -p /opt/flotilla/data
@@ -32,8 +41,6 @@ for module in $GITROOT/config/*/; do
     dest="/opt/flotilla/config/$(basename "$module")"
     elevated_link_source $module $dest
 done
-
-elevate mkdir -p /opt/flotilla/config/heimdall/keys/
 
 for service in $GITROOT/services/*; do
     dest="/etc/systemd/system/$(basename "$service")"
@@ -45,6 +52,21 @@ done
 elevate systemctl daemon-reload
 
 elevated_link_source "${GITROOT}/alpha/docker-compose.yml" "/opt/flotilla/docker-compose.yml"
+
+# Setup OpenVPN configuration.
+if [[ -d /opt/flotilla/config/openvpn ]] && [[ ! -d /opt/flotilla/config/openvpn/backup ]]; then
+    if [[ "$(probe chcon)" -eq 1 ]]; then
+        # SELinux needs some allowances.
+        wget https://raw.githubusercontent.com/kylemanna/docker-openvpn/master/docs/docker-openvpn.te -O /tmp/docker-openvpn.te
+        checkmodule -M -m -o /tmp/docker-openvpn.mod /tmp/docker-openvpn.te
+        semodule_package -o /tmp/docker-openvpn.pp -m /tmp/docker-openvpn.mod
+        elevate semodule -i /tmp/docker-openvpn.pp
+        elevate modprobe tun
+    fi
+
+    elevate docker run -v /opt/flotilla/config/openpvn:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://vpn.maxocull.com
+    elevate docker run -v /opt/flotilla/config/openpvn:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
+fi
 
 # Allow permission to write to volumes in SE Linux.
 elevate chcon -RLt svirt_sandbox_file_t /opt/flotilla
