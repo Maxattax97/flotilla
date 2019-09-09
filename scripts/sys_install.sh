417c2d563aa42707812f2c0cda862c0f56017670
Max O'Cull <max.ocull@protonmail.com>
2019-09-08 21:39:52 -0400

    First minimal docker-compose working

--- scripts/sys_install.sh
+++ scripts/sys_install.sh
@@ -1,14 +1,50 @@
 #!/usr/bin/env bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-source "${SCRIPTPATH}/sys_lib.sh"
+GITROOT="$(dirname "${SCRIPTPATH}")"
 
-elevate
+if [ -s "${SCRIPTPATH}/sys_lib.sh" ]; then
+    source "${SCRIPTPATH}/sys_lib.sh"
+else
+    source "/usr/local/lib/helm/scripts/sys_lib.sh"
+fi
+
+elevate mkdir -p /usr/local/lib/helm/
+elevated_link_source "${GITROOT}/scripts/" "/usr/local/lib/helm/scripts"
+elevated_link_source "${GITROOT}/helm" "/usr/local/bin/helm"
 
 if [[ "$(probe dnf)" -eq 1 ]]; then
-    dnf install -y \
-        docker tmux neovim
+    elevate dnf upgrade -y
+    elevate dnf install -y \
+        docker docker-compose tmux neovim nodejs
 elif [[ "$(probe apt)" -eq 1 ]]; then
-    apt install -y \
+    elevate apt update
+    elevate apt upgrade -y
+    elevate apt install -y \
         docker tmux neovim
 fi
+
+elevate mkdir -p /opt/flotilla/data
+elevate mkdir -p /opt/flotilla/config
+
+for module in $GITROOT/config/*/; do
+    dest="/opt/flotilla/config/$(basename "$module")"
+    elevated_link_source $module $dest
+done
+
+elevate mkdir -p /opt/flotilla/config/heimdall/keys/
+
+for service in $GITROOT/services/*; do
+    dest="/etc/systemd/system/$(basename "$service")"
+    # Must copy because symlinks to home fail.
+    echo "Copying $service -> $dest ..."
+    elevate cp -f $service $dest
+done
+
+elevate systemctl daemon-reload
+
+elevated_link_source "${GITROOT}/alpha/docker-compose.yml" "/opt/flotilla/docker-compose.yml"
+
+# Allow permission to write to volumes in SE Linux.
+elevate chcon -RLt svirt_sandbox_file_t /opt/flotilla
+elevate chown -RLf nobody:nobody /opt/flotilla # "${USER}:${USER}" /opt/flotilla
--- scripts/sys_lib.sh
+++ scripts/sys_lib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 
 silence() {
     eval "$@" > /dev/null 2>&1
@@ -30,4 +30,30 @@ elevate() {
             exit 1
         fi
     fi
+}
+
+# NOTE: The trailing slash MATTERS!
+# link_source "/home/max/config/i3/" "/home/max/.config/i3"
+link_source() {
+    src="${1}"
+    dest="${2}"
+    elevated="${3}"
+    if [ -h "$dest" ]; then
+        echo "Skipping $dest because it is already linked ..."
+    elif [ -f "$dest" ]; then
+        echo "Skipping $dest because a file exists there ..."
+    elif [ -d "$dest" ]; then
+        echo "Skipping $dest because a directory exists there ..."
+    else
+        echo "Linking $src -> $dest ..."
+        if [ "${elevated}" -ne 0 ]; then
+            elevate ln -sf "$src" "$dest"
+        else
+            ln -sf "$src" "$dest"
+        fi
+    fi
+}
+
+elevated_link_source() {
+    link_source "${1}" "${2}" "1"
 }
